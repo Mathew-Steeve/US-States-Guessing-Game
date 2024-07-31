fec586e8503d3fa720edc427efc0368b984e7144
Mathew_Steeve <mthwsteeve@gmail.com>
2024-07-31 22:10:14 +0530

    States name guessing game created

---
+++ main.py
@@ -0,0 +1,32 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game ")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+State_list = data.state.to_list()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer = screen.textinput(f"{len(guessed_states)}/50 states correct", "What is your guess? ").title()
+    # print(answer)
+
+    if answer == "Exit" :
+        states_to_learn = []
+        for states in State_list:
+            if states not in guessed_states:
+                states_to_learn.append(states)
+        learn = pandas.DataFrame(states_to_learn)
+        learn.to_csv("States_to_learn.csv")
+        break
+    if answer in State_list:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.ht()
+        t.penup()
+        State = data[data.state == answer]
+        t.goto(State.x.item(), State.y.item())
+        t.write(answer)
+
